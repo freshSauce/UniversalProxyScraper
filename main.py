a0409c4f25c07d1f2f116ff98c8519b53e3d7fef
freshSauce <nex0loke@gmail.com>
2022-01-31 20:21:33 -0600

    Fixed infinite loop with Merge #3.

--- main.py
+++ main.py
@@ -6,8 +6,9 @@ import concurrent.futures
 import base64
 from exceptions import *
 from specific.spys_one import Deofuscator
-from random import choice
+from random import shuffle
 import argparse
+from math import floor
 
 logo = '''
 ██╗   ██╗   ██████╗ ███████╗    █████╗  ██████╗ 
@@ -20,46 +21,72 @@ logo = '''
             Proxy
 Universal           Scraper | Your ideal proxy scraper ;)
        by: @freshSauce
-           0.1.5
+           0.1.6
 '''
 
 
-def __ArgsBuilder():
+def __argsBuilder():
     """
     This function works as the builder of the args that 
     """
-    parser = argparse.ArgumentParser(description='Command-line option for the Universal Scraper')
+    parser = argparse.ArgumentParser(
+        description='Command-line option for the Universal Scraper'
+    )
 
-    parser.add_argument('-f', '--file', type=str, action='store', required=True,
-                    help='name of the file with the sites')
-    parser.add_argument('-o', '--output', action='store_true',
-                    help='if used, stores the scraped proxies')
-    parser.add_argument('-q', '--quantity', type=int, action='store', default=10,
-                    help='if used, stores the scraped proxies')
-    parser.add_argument('-v', '--verify', action='store_true',
-                    help='if used, verify every single proxy and returns the live ones')
-    parser.add_argument('-p', '--print', action='store_true',
-                    help='if used, prints out the obtained list of proxies')
+    parser.add_argument(
+        '-f',
+        '--file',
+        type=str,
+        action='store',
+        required=True,
+        help='name of the file with the sites'
+    )
+    parser.add_argument(
+        '-o',
+        '--output',
+        action='store_true',
+        help='if used, stores the scraped proxies'
+    )
+    parser.add_argument(
+        '-q',
+        '--quantity',
+        type=int,
+        action='store',
+        default=10,
+        help='if used, stores the scraped proxies'
+    )
+    parser.add_argument(
+        '-v',
+        '--verify',
+        action='store_true',
+        help='if used, verify every single proxy and returns the live ones'
+    )
+    parser.add_argument(
+        '-p',
+        '--print',
+        action='store_true',
+        help='if used, prints out the obtained list of proxies'
+    )
 
     args = vars(parser.parse_args())
 
     return args
 
+
 class ProxyScraper:
     HTML = str
 
     def __init__(
-                self, 
-                input: Union[str, list], 
-                output: bool = None,
-                check: bool = None
-                ) -> None:
-
+        self,
+        input: Union[str, list],
+        output: bool = None,
+        check: bool = None
+    ) -> None:
         """
         Parameters
         ----------
-        file : str
-            The name of the file with the sites
+        input : str
+            Sites to be scraped
         output : bool, optional
             In case that the user needs the output he can store it
             on a file named 'output.txt'.
@@ -71,8 +98,9 @@ class ProxyScraper:
         self.output = output
         self.check = check
         self.headers = {
-                        'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/532.5 (KHTML, like Gecko) Chrome/4.0.249.0 Safari/532.5'
-                        }
+            'User-Agent':
+                'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/532.5 (KHTML, like Gecko) Chrome/4.0.249.0 Safari/532.5'
+        }
 
     def __openFile(self) -> list[str]:
         """
@@ -89,14 +117,9 @@ class ProxyScraper:
         except FileNotFoundError:
             exit(
                 f'An error ocurred while trying to open file {self.input}, please, make sure you typed correctly your file\'s name'
-                )
-            
+            )
 
-
-    def __saveFile(
-                    self, 
-                    proxy_list: list
-                    ) -> None:
+    def __saveFile(self, proxy_list: list) -> None:
         """
         Parameters
         -----------
@@ -110,14 +133,11 @@ class ProxyScraper:
         if self.output:
             proxies = [proxy + '\n' for proxy in proxy_list]
             with open('output.txt', 'w') as output:
-               output.writelines(proxies) 
+                output.writelines(proxies)
         else:
             pass
-    
-    def __doRequest(
-                    self, 
-                    url: str
-                    )-> str: 
+
+    def __doRequest(self, url: str) -> Union[bool, dict]:
         """
         Parameters
         ----------
@@ -131,14 +151,11 @@ class ProxyScraper:
         """
 
         request = urllib3.PoolManager()
-        
+
         try:
             result = request.request(
-                                        'GET', 
-                                        url, 
-                                        timeout=1.5,
-                                        headers=self.headers
-                                    )
+                'GET', url, timeout=1.5, headers=self.headers
+            )
 
         except urllib3.exceptions.MaxRetryError:
             print(f'Connection to {url.strip()} timed out ')
@@ -150,10 +167,7 @@ class ProxyScraper:
         else:
             return False
 
-    def __poolCreator(
-                        self, 
-                        urls: list[str]
-                    ) -> list[str]:
+    def __poolCreator(self, urls: list[str]) -> list[Union[bool, dict]]:
         """
         Params
         ------
@@ -168,14 +182,16 @@ class ProxyScraper:
         html_list = list()
 
         with concurrent.futures.ThreadPoolExecutor() as executor:
-            thread_list = [executor.submit(self.__doRequest, url) for url in urls]
+            thread_list = [
+                executor.submit(self.__doRequest, url) for url in urls
+            ]
             for result in concurrent.futures.as_completed(thread_list):
                 if result.result():
                     html_list.append(result.result())
-            
+
         return html_list
 
-    def __scrape(self, html: HTML, site: str) -> list[str]:
+    def __scrape(self, html: HTML, site: str, quantity: int) -> list[str]:
         """
         Parameters
         ----------
@@ -184,7 +200,7 @@ class ProxyScraper:
 
         Return
         ------
-        list[str] : 
+        list[str] :
             List with every proxy obtained from the scraped data.
         """
         if 'https://spys.one/en' == site:
@@ -192,7 +208,9 @@ class ProxyScraper:
             try:
                 table_body = re.search(pattern, html.strip()).group()
             except AttributeError:
-                raise InternalError("Whoops! Seems like something wrong happened. Please, open an Issue including the URLs your're trying to scrape!")
+                raise InternalError(
+                    "Whoops! Seems like something wrong happened. Please, open an Issue including the URLs you're trying to scrape!"
+                )
 
             pattern = r'<tr.*?<\/tr>'
 
@@ -200,24 +218,35 @@ class ProxyScraper:
 
             proxy_list = list()
 
-            script = re.search(r'eval.*', html).group() 
+            script = re.search(r'eval.*', html).group()
             pattern = r'\(\'[\w].+?\}\)'
 
-            variables_raw = re.search(pattern, script).group() 
+            variables_raw = re.search(pattern, script).group()
 
-            variables = variables_raw.lstrip('(').rstrip(')').replace('\\u005e', '^').split(',')
+            variables = variables_raw.lstrip('(').rstrip(')').replace(
+                '\\u005e', '^'
+            ).split(',')
 
-            p, r, o, x, y, s = variables[0].replace("'", ''), variables[1], variables[2], variables[3], variables[4], variables[5]
+            p, r, o, x, y, s = variables[0].replace("'", ''), variables[1], variables[2], variables[3], variables[4], \
+                               variables[5]
 
             for row in table_rows[2:]:
-                elements_raw = re.findall(r'<td.*?<\/td>', row.group())
-                elements = [re.sub(r'<.*?>', '', element).strip() for element in elements_raw]
+                if len(proxy_list) == floor(quantity * 1.5):
+                    break
+                elements_raw = re.findall(r'<td.*?</td>', row.group())
+                elements = [
+                    re.sub(r'<.*?>', '', element).strip()
+                    for element in elements_raw
+                ]
 
                 ip_port_cleaned = re.sub(r'(\'&nbsp;)|(\)\')|', '', elements[0])
 
-                ip_port_splited = re.split(r'document\.write\(\":\"\+', ip_port_cleaned)
+                ip_port_splited = re.split(
+                    r'document\.write\(\":\"\+', ip_port_cleaned
+                )
 
-                ip, port = ip_port_splited[0].replace('&nbsp;', ''), ip_port_splited[1]
+                ip, port = ip_port_splited[0].replace('&nbsp;',
+                                                      ''), ip_port_splited[1]
 
                 port = Deofuscator(p, r, o, x, y, s).deofuscator(port)
 
@@ -238,8 +267,13 @@ class ProxyScraper:
         proxy_list = list()
 
         for row in table_rows:
-            elements_raw = re.findall(r'<td.*?<\/td>', row.group())
-            elements = [re.sub(r'<.*?>', '', element).strip() for element in elements_raw]
+            if len(proxy_list) == floor(quantity * 1.5):
+                break
+            elements_raw = re.findall(r'<td.*?</td>', row.group())
+            elements = [
+                re.sub(r'<.*?>', '', element).strip()
+                for element in elements_raw
+            ]
 
             try:
                 ip, port = elements[0], elements[1]
@@ -251,7 +285,10 @@ class ProxyScraper:
                 ip = re.sub(r'(.*[^=\w\d]\")|(\"\).*)', '', ip)
                 port = re.sub(r'(.*[^=\w\d]\")|(\"\).*)', '', port)
 
-                if not bool(re.search(r'[\d]{0,3}\.[\d]{0,3}\.[\d]{0,3}\.[\d]{0,3}', ip)):
+                if not bool(
+                    re.
+                    search(r'[\d]{0,3}\.[\d]{0,3}\.[\d]{0,3}\.[\d]{0,3}', ip)
+                ):
                     try:
                         ip = base64.b64decode(ip).decode('utf-8')
                     except UnicodeDecodeError:
@@ -261,7 +298,7 @@ class ProxyScraper:
 
                 if not bool(re.search(r'[\d]{0,5}', port)):
                     try:
-                     port = base64.b64decode(port).decode('utf-8')
+                        port = base64.b64decode(port).decode('utf-8')
                     except UnicodeDecodeError:
 
                         port_raw = urllib.parse.unquote(port)
@@ -271,9 +308,9 @@ class ProxyScraper:
                 ip = re.sub(r'(.*\(\')|(\'\);)', '', ip)
                 port = re.sub(r'(.*\(\')|(\'\);)', '', port)
             proxy_list.append(f'{ip}:{port}')
-        
+
         return proxy_list
-    
+
     def __checker(self, proxy: str) -> bool:
         """
         Parameters
@@ -290,17 +327,14 @@ class ProxyScraper:
 
         try:
             request.request('GET', 'https://httpbin.org/get', timeout=1.5)
-            return True
-        except (urllib3.exceptions.ProxyError, urllib3.exceptions.MaxRetryError):
-             return False
+            return {"live": True, "proxy": proxy}
 
-           
+        except (
+            urllib3.exceptions.ProxyError, urllib3.exceptions.MaxRetryError
+        ):
+            return {"live": False, "proxy": proxy}
 
-
-    def Proxies(
-                self, 
-                quantity: int = 10
-                ) -> list[str]:
+    def Proxies(self, quantity: int = 10) -> list[str]:
         """
         Parameters
         ----------
@@ -313,12 +347,12 @@ class ProxyScraper:
         list[str] :
             List with every proxy scraped.
         """
-        urls = self.__openFile() if type(self.input) == "str" else self.input
+        urls = self.__openFile() if type(self.input) == "list" else self.input
 
         if not urls:
             raise ArgumentNotProvided(
-                                        "Whoops! seems like you've forgot to provide the URLs !!!"
-                                    )
+                "Whoops! seems like you've forgot to provide the URLs !!!"
+            )
 
         html_content_list = self.__poolCreator(urls)
 
@@ -326,56 +360,49 @@ class ProxyScraper:
         for html in html_content_list:
             proxy_list = self.__scrape(html['html'], html['site'])
             proxies += proxy_list
-            
-        if quantity > len(proxies):
-            if self.output == True:
-                self.__saveFile(proxies)
 
-            if self.check == True:
-                proxies = list()
-                for proxy in proxy_list:
-                    if self.__checker(proxy):
-                        proxies.append(proxy)
+        if self.check:
+            liveProxies = list()
+            with concurrent.futures.ThreadPoolExecutor() as executor:
+                results = executor.map(self.__checker, proxies)
+                for result in results:
+                    if result["live"]:
+                        liveProxies.append(result["proxy"])
+            proxies = liveProxies
 
-        else:
-            _proxies = list()
-            for _ in range(quantity):
-                proxy = choice(proxies)
-                if proxy not in _proxies:
-                    _proxies.append(proxy)
-            if self.output == True:
-                self.__saveFile(_proxies)
-            
-            if self.check == True:
-                proxies = list()
-                for proxy in _proxies:
-                    if self.__checker(proxy):
-                        proxies.append(proxy)
+        # Append the quantity of proxies that will be returned
+        if quantity < len(proxies):
+            shuffle(proxies)
+            proxies = proxies[:quantity]
 
-            proxies = _proxies
+        if self.outpute:
+            self.__saveFile(proxies)
 
         return proxies
+
 
 if __name__ == '__main__':
 
     print(logo)
-    args = __ArgsBuilder()
+    args = __argsBuilder()
 
     while True:
-        result = ProxyScraper(args['file'], args['output'], args['verify']).Proxies(args['quantity'])
+        result = ProxyScraper(args['file'], args['output'],
+                              args['verify']).Proxies(args['quantity'])
         print('Proxies obtained !!!')
-        
+
         if args['print']:
             print(result)
         while True:
-            option = input('Everything is done !!! Wanna get more proxies? (Y[es]/N[o]): ').lower()
+            option = input(
+                'Everything is done !!! Wanna get more proxies? (Y[es]/N[o]): '
+            ).lower()
 
             if option == 'y' or option == 'yes':
                 break
-                    
+
             elif option == 'n' or option == 'no':
                 exit('Have a nice day !!!')
 
             else:
                 print('Please, use Y[es]/N[o] only !!!')
-            
